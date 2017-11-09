7afff59578254e5abe10cbe4fa406bf60497c03f
Peter Haro <peter.haro@sintef.no>
2017-11-09 14:51:44 +0100

    Quick fixing while I got time

--- OpenSeaLab/blueprints/h2020_blueprint.py
+++ OpenSeaLab/blueprints/h2020_blueprint.py
@@ -1,25 +1,28 @@
 import os
+import csv
+import json
 
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-templates_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../templates')
-horizon_blueprint = Blueprint('h2020_blueprint', __name__,
+templates_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../templates")
+horizon_blueprint = Blueprint("h2020_blueprint", __name__,
                               template_folder=templates_directory)
 
 print(templates_directory)
 
 
-@horizon_blueprint.route('/')
+@horizon_blueprint.route("/")
 def show():
     try:
-        return render_template('digitize_or_die.html')
+        return render_template("digitize_or_die.html")
     except TemplateNotFound:
         abort(404)
 
 
-@horizon_blueprint.route('/get_data')
+@horizon_blueprint.route("/get_data")
 def get_data():
-    import pandas as pd
-    df = pd.read_csv("../static/H2020.csv")
-    return df.to_json()
+    with open("./OpenSeaLab/static/H2020.csv", newline="", encoding="utf-8") as data:
+        reader = csv.reader(data, delimiter=",")
+        retval = [item for item in reader]
+        return json.dumps(retval)
--- OpenSeaLab/static/javascript/h2020_test.js
+++ OpenSeaLab/static/javascript/h2020_test.js
@@ -1,30 +1,37 @@
 var debug = true;
 var proposalsInProgramBarChart = dc.rowChart("#programme-bar-chart");
+var actionTypePieChart = dc.pieChart("#action-type-pie-chart");
 
 // LOAD DATA
 
-$.getJSON("/get_data", {}).done(function (data) {
-    $.each(data.items, function (i, item) {
-
-    });
+$.getJSON("/h2020/get_data", {}).done(function (data) {
+    data.shift(); //Remove CSV header
     var ndx = crossfilter(data);
     var all = ndx.groupAll();
 
+    //0 = programme
     var programmeNameDimension = ndx.dimension(function (d) {
-        return d.Programme;
+        return d[0];
     });
-    var programmeGroup = programmeNameDimension.group();
+    var actionTypeDimension = ndx.dimension(function (d) {
+        if(!d[5]) {
+            return "";
+        }
+        return d[5].split("-");
+    });
 
-    proposalsInProgramBarChart/* dc.rowChart('#day-of-week-chart', 'chartGroup') */
-        .width(180)
-        .height(180)
+    var programmeGroup = programmeNameDimension.group().reduceCount();
+    var actionTypeGroup = actionTypeDimension.group().reduceCount();
+    proposalsInProgramBarChart
+        .width(360)
+        .height(360)
         .margins({top: 20, left: 10, right: 10, bottom: 20})
         .group(programmeGroup)
         .dimension(programmeNameDimension)
         // Assign colors to each value in the x scale domain
         .ordinalColors(['#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb'])
         .label(function (d) {
-            return d.key.split('.')[1];
+            return d.key;
         })
         // Title sets the row text
         .title(function (d) {
@@ -33,16 +40,24 @@ $.getJSON("/get_data", {}).done(function (data) {
         .elasticX(true)
         .xAxis().ticks(4);
 
+    actionTypePieChart.width(180)
+        .height(180)
+        .radius(80)
+        .innerRadius(30)
+        //.margins({top: 0, right: 50, bottom: 20, left: 40})
+        .dimension(actionTypeDimension)
+        .group(actionTypeGroup);
+
 
     //simply call `.renderAll()` to render all charts on the page
     dc.renderAll();
     /*
-    // Or you can render charts belonging to a specific chart group
-    dc.renderAll('group');
-    // Once rendered you can call `.redrawAll()` to update charts incrementally when the data
-    // changes, without re-rendering everything
-    dc.redrawAll();
-    // Or you can choose to redraw only those charts associated with a specific chart group
-    dc.redrawAll('group');
-    */
+     // Or you can render charts belonging to a specific chart group
+     dc.renderAll('group');
+     // Once rendered you can call `.redrawAll()` to update charts incrementally when the data
+     // changes, without re-rendering everything
+     dc.redrawAll();
+     // Or you can choose to redraw only those charts associated with a specific chart group
+     dc.redrawAll('group');
+     */
 });
